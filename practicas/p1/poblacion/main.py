a1e940d13b08895497f67b61289170fd25a83bcd
miguel <migue8gl@gmail.com>
2023-11-02 17:48:46 +0100

    Added main.py script

---
+++ practicas/p1/poblacion/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  2 17:45:22 2023
+
+@author: migue8gl
+"""
+
+import R1
+import R2
+import R3
+import R4
+import R5
+
+
+def ejecutar():
+    R1.ejecutar()
+    R2.ejecutar()
+    R3.ejecutar()
+    R4.ejecutar()
+    R5.ejecutar()
+
+
+if __name__ == "__main__":
+    ejecutar()
